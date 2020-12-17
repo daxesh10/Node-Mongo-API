e811b67c8f3719fe64c8db03ff41c316c736b073
daxesh mehra <daxshmehra10@gmail.com>
2020-12-17 13:40:20 -0800

    api changes order

--- routes/orders.js
+++ routes/orders.js
@@ -86,4 +86,27 @@ router.post("/", (req, res, next) => {
     });
 });
 
+
+router.get("/", (req, res, next) => {
+  Order
+  .find()
+  .exec()
+  .then(docs => {
+    console.log(docs);
+      if (docs.length >= 0) {
+    res.status(200).json(docs);
+      } else {
+          res.status(404).json({
+              message: 'No entries found'
+          });
+      }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: err
+    });
+  });
+});
+
 module.exports = router;
